3c5276d09ee991a6283c10154a36b836369b42fb
Adele <bidemidone@gmail.com>
2023-05-24 13:12:35 -0700

    fixed the compiler error from the getinfo.c

--- getinfo.c
+++ getinfo.c
@@ -11,6 +11,34 @@ info->path = NULL;
 info->argc = 0;
 }
 /**
+* set_info - initializes info_t struct
+* @info: struct address
+* @av: argument vector
+*/
+void set_info(info_t *info, char **av)
+{
+int i = 0;
+info->fname = av[0];
+if (info->arg)
+{
+info->argv = strtow(info->arg, " \t");
+if (!info->argv)
+{
+info->argv = malloc(sizeof(char *) * 2);
+if (info->argv)
+{
+info->argv[0] = _strdup(info->arg);
+info->argv[1] = NULL;
+}
+}
+for (i = 0; info->argv && info->argv[i]; i++)
+;
+info->argc = i;
+alias_replacement(info);
+replace_vars(info);
+}
+}
+/**
 * free_info - frees info_t struct fields
 * @info: struct address
 * @all: true if freeing all fields
@@ -38,3 +66,4 @@ close(info->readfd);
 putchar_char(BUF_FLUSH);
 }
 }
+
