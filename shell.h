0bbe85f549c88b7dc4d7b3f0f3ab5f8b9f553463
Mpho Matli <mphojames9@gmail.com>
2023-05-24 18:36:46 -0700

    update

--- shell.h
+++ shell.h
@@ -1,32 +1,29 @@
 #ifndef _SHELL_H_
 #define _SHELL_H_
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
 #include <limits.h>
 #include <fcntl.h>
 #include <errno.h>
-
-#define READ_BUF_SIZE 1024
-#define WRITE_BUF_SIZE 1024
-#define BUF_FLUSH -1
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 #define CMD_NORM 0
 #define CMD_OR 1
 #define CMD_AND 2
 #define CMD_CHAIN 3
-
 #define CONVERT_LOWERCASE 1
 #define CONVERT_UNSIGNED 2
-
 #define USEgetline_checker 0
 #define USE_STRTOK 0
 #define HIST_FILE ".simple_shell_history"
 #define HIST_MAX 4096
+#define READ_BUF_SIZE 1024
+#define WRITE_BUF_SIZE 1024
+#define BUF_FLUSH -1
+
 extern char **environ;
 /**
 * struct liststr - singly linked list
