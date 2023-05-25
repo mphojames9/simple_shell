9390ed8715dc8cf5306569db75a8c7eb6e5d524d
Mpho Matli <mphojames9@gmail.com>
2023-05-24 18:19:58 -0700

    shell update

--- shell.h
+++ shell.h
@@ -41,27 +41,27 @@ char *str;
 struct liststr *next;
 } record_m;
 /**
-* struct passinfo - contains pseudo-arguements to pass into a function,
-* allowing uniform prototype for function pointer struct
-* @arg: a string generated from getline containing arguements
-* @argv:an array of strings generated from arg
-* @path: a string path for the current command
-* @argc: the argument count
-* @line_count: the error count
-* @err_num: the error code for exit()s
-* @linecount_flag: if on count this line of input
-* @fname: the program filename
-* @env: linked list local copy of environ
-* @environ: custom modified copy of environ from LL env
-* @history: the history node
-* @alias: the alias node
-* @env_changed: on if environ was changed
-* @status: the return status of the last exec'd command
-* @cmd_buf: address of pointer to cmd_buf, on if chaining
+* struct passinfo - have pseudo-arguements that pases the function
+* @arg: a string
+* @argv:an strings created from arg
+* @path: current command string paff
+* @argc: argument count
+* @line_count: mistake count
+* @err_num: the mistake code for exit()s
+* @linecount_flag: line of input
+* @fname: program filename
+* @env: copy of environ for linked list local
+* @environ: modified copy of environ from LL env
+* @history: history node
+* @alias: alias node
+* @env_changed: if environ was changed
+* @status:last exec'd command return status
+* @cmd_buf: cmd_buf, on if chaining address of pointer
 * @cmd_buf_type: CMD_type ||, &&, ;
-* @readfd: the fd from which to read line input
-* @histcount: the history line number count
+* @readfd: read line input
+* @histcount: history line num count
 */
+
 typedef struct passinfo
 {
 char *arg;
@@ -78,8 +78,8 @@ record_m *alias;
 char **environ;
 int env_changed;
 int status;
-char **cmd_buf; /* pointer to cmd ; chain buffer, for memory mangement */
-int cmd_buf_type; /* CMD_type ||, &&, ; */
+char **cmd_buf;
+int cmd_buf_type;
 int readfd;
 int histcount;
 } info_t;
@@ -95,14 +95,36 @@ char *type;
 int (*func)(info_t *);
 } builtin_table;
 
+size_t list_len(const record_m *);
+char **record_mo_strings(record_m *);
+size_t print_list(const record_m *);
+record_m *node_starts_with(record_m *, char *, char);
+ssize_t get_node_index(record_m *, record_m *);
+
+record_m *add_node(record_m **, const char *, int);
+record_m *add_node_end(record_m **, const char *, int);
+size_t print_list_str(const record_m *);
+int node_at_index_deletion(record_m **, unsigned int);
+void free_list(record_m **);
+
 int hsh(info_t *, char **);
 int builtin_locator(info_t *);
 void find_cmd(info_t *);
 void fork_cmd(info_t *);
 
+char **get_environ(info_t *);
+int unsetenv2_checker(info_t *, char *);
+int setenv2_checker(info_t *, char *, char *);
+
 int cmd_checker(info_t *, char *);
 char *dup_chars(char *, int, int);
 char *find_path(info_t *, char *, char *);
+
+char *get_history_file(info_t *info);
+int history_writer(info_t *info);
+int history_reader(info_t *info);
+int history_list_builder(info_t *info, char *buf, int linecount);
+int history_renumber(info_t *info);
 
 int hsh_loop(char **);
 
@@ -115,6 +137,12 @@ int strlen_char(char *);
 int strcmp_char(char *, char *);
 char *starts_with(const char *, const char *);
 char *_strcat(char *, char *);
+
+int chain_checker(info_t *, char *, size_t *);
+void check_chain(info_t *, char *, size_t *, size_t, size_t);
+int alias_replacement(info_t *);
+int replace_vars(info_t *);
+int string_replacement(char **, char *);
 
 char *_strcpy(char *, char *);
 char *_strdup(const char *);
@@ -165,32 +193,4 @@ int env_checker(info_t *);
 int setenv_checker(info_t *);
 int unsetenv_checker(info_t *);
 int env_list_population(info_t *);
-
-char **get_environ(info_t *);
-int unsetenv2_checker(info_t *, char *);
-int setenv2_checker(info_t *, char *, char *);
-
-char *get_history_file(info_t *info);
-int history_writer(info_t *info);
-int history_reader(info_t *info);
-int history_list_builder(info_t *info, char *buf, int linecount);
-int history_renumber(info_t *info);
-
-record_m *add_node(record_m **, const char *, int);
-record_m *add_node_end(record_m **, const char *, int);
-size_t print_list_str(const record_m *);
-int node_at_index_deletion(record_m **, unsigned int);
-void free_list(record_m **);
-
-size_t list_len(const record_m *);
-char **record_mo_strings(record_m *);
-size_t print_list(const record_m *);
-record_m *node_starts_with(record_m *, char *, char);
-ssize_t get_node_index(record_m *, record_m *);
-
-int chain_checker(info_t *, char *, size_t *);
-void check_chain(info_t *, char *, size_t *, size_t, size_t);
-int alias_replacement(info_t *);
-int replace_vars(info_t *);
-int string_replacement(char **, char *);
 #endif
