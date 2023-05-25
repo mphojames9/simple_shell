4f951f8160f07e72b752810a5a4fb7253d313b01
bidemidone1 <99288950+bidemidone1@users.noreply.github.com>
2023-05-25 15:16:47 +0100

    Update exits-out.c

--- exits-out.c
+++ exits-out.c
@@ -1,10 +1,10 @@
 #include "shell.h"
 /**
-**_strncpy - copies a string
-*@dest: the destination string to be copied to
-*@src: the source string
-*@n: the amount of characters to be copied
-*Return: the concatenated string
+**_strncpy - duplicates string
+*@dest: destination string copied into
+*@src: source-string
+*@n: amount of characters for copying
+*Return: concatenated string form
 */
 char *_strncpy(char *dest, char *src, int n)
 {
@@ -28,11 +28,11 @@ j++;
 return (s);
 }
 /**
-**_strncat - concatenates two strings
-*@dest: the first string
-*@src: the second string
-*@n: the amount of bytes to be maximally used
-*Return: the concatenated string
+**_strncat - concatenates two-strings
+*@dest: the first-string
+*@src: the second-string
+*@n: quantity of bytes to be maximally used
+*Return: the concatenated string as given
 */
 char *_strncat(char *dest, char *src, int n)
 {
@@ -53,10 +53,10 @@ dest[i] = '\0';
 return (s);
 }
 /**
-**_strchr - locates a character in a string
-*@s: the string to be parsed
-*@c: the character to look for
-*Return: (s) a pointer to the memory area s
+**_strchr - locates character in string
+*@s: string to be parsed parsee
+*@c: character to look out for
+*Return: (s) a pointer to the memory area s as given
 */
 char *_strchr(char *s, char c)
 {
