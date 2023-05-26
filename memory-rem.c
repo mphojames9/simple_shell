252e94bcb31e15b98f640866a8fb45b3966ce34d
Adele <bidemidone@gmail.com>
2023-05-26 11:40:58 -0700

    updated

--- memory-rem.c
+++ memory-rem.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * befree - NULLifies the address to the location pointer is freed
+ * befree - NULLifies the addres to the location pointer is freed
  * @ptr: pointer's address that needs freeing
  * Return: 1 if succesfully, 0 om failer
  */
